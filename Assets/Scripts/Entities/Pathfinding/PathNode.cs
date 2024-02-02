3c60f2fc3f18e8d0d47365728eb7860781655b56
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2024-02-02 15:30:37 +0900

    #9 feat: 길찾기 알고리즘 개선
    1. Pathfinding :
    - 휴리스틱을 맨허튼 방식으로 변경 및 가중치 추가
    - 대각선으로 이동할 경우 코너를 인식
    - 함수 이름 변경 및 불필요한 코드 개선
    
    2. PathNode :
    - IsClosed 삭제, Pathfinding에서 따로 관리

--- Assets/Scripts/Entities/Pathfinding/PathNode.cs
+++ Assets/Scripts/Entities/Pathfinding/PathNode.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class PathNode
 {
     public Vector2Int Pos;
-    public bool IsClosed;
     public bool IsBlocked;
 
     public int G;
@@ -24,7 +23,6 @@ public class PathNode
         G = 0;
         H = 0;
         F = 0;
-        IsClosed = false;
         BeforeNode = null;
         QueueIndex = 0;
     }
--- Assets/Scripts/Entities/Pathfinding/Pathfinding.cs
+++ Assets/Scripts/Entities/Pathfinding/Pathfinding.cs
@@ -5,6 +5,7 @@ public class Pathfinding : MonoBehaviour
 {
     private const int Move_STARIGHT_COST = 10;
     private const int Move_DIAGONAL_COST = 14;
+    [SerializeField] private int _weight;
 
     private PathNode[,] _nodes;
     private NodePriorityQueue _openList;
@@ -12,32 +13,33 @@ public class Pathfinding : MonoBehaviour
     private Stack<PathNode> _stack;
     private PathNode _current;
 
-    public Vector2Int Start;
-    public Vector2Int Goal;
+    private Vector2Int _nodeMapSize;
+    private Vector2Int _goal;
 
     public List<Vector2Int> DebugList;
 
     public void Awake()
     {
-        DebugList.Clear();
-        _nodes = new PathNode[200, 200];
-        for (int i = 0; i < _nodes.GetLength(0); i++)
-        {
-            for (int k = 0; k < _nodes.GetLength(1); k++)
-            {
-                _nodes[i, k] = new PathNode(new Vector2Int(i, k));
-            }
-        }
-        _stack = new();
-        DebugList = new();
-        _openList = new NodePriorityQueue(1500);
-        _closeList = new HashSet<PathNode>();
+        _stack = new(256);
+        DebugList = new(256);
+        _openList = new NodePriorityQueue(1024);
+        _closeList = new HashSet<PathNode>(1024);
     }
 
-    public void FindPath(Vector2Int start, Vector2Int goal)
+    public void SetNodeMap(PathNode[,] nodes)
     {
+        _nodes = nodes;
+        _nodeMapSize = new(_nodes.GetLength(0), _nodes.GetLength(1));
+    }
+
+    public Stack<PathNode> FindPath(Vector2Int start, Vector2Int goal)
+    {
+        if (_nodes[goal.x, goal.y].IsBlocked
+         || _nodes[start.x, start.y].IsBlocked)
+            return _stack;
+
         ResetPathNodeData();
-        Goal = goal;
+        _goal = goal;
         _openList.Enqueue(_nodes[start.x, start.y]);
 
         while (_openList.Count > 0)
@@ -45,27 +47,26 @@ public class Pathfinding : MonoBehaviour
             _current = _openList.Dequeue();
             if (_current.Pos == goal)
             {
-                LinkPath(_current);
+                ConnectPathNode(_current);
                 GizmoTest();
                 break;
             }
             AddAroundPathNode(_current);
             _closeList.Add(_current);
         }
+
+        return _stack;
     }
 
     private void ResetPathNodeData()
     {
-        int lengthX = _nodes.GetLength(0);
-        int lengthY = _nodes.GetLength(1);
-
-        for (int x = 0; x < lengthX; x++)
+        foreach (var node in _closeList)
         {
-            for (int y = 0; y < lengthY; y++)
-            {
-                _nodes[x, y].ResetPathfindingData();
-            }
+            node.ResetPathfindingData();
         }
+        _closeList.Clear();
+        _stack.Clear();
+        DebugList.Clear();
     }
 
     private void AddAroundPathNode(PathNode target)
@@ -78,81 +79,69 @@ public class Pathfinding : MonoBehaviour
         int bottomY = centerY - 1;
         int topY = centerY + 1;
 
-        GetDiagonalNode(leftX, bottomY); // 1
-        GetDiagonalNode(leftX, topY); // 7
-        GetDiagonalNode(rightX, bottomY); // 3
-        GetDiagonalNode(rightX, topY); // 9
+        TryAddPathNode(leftX, centerY, Move_STARIGHT_COST);        // 4
+        TryAddPathNode(rightX, centerY, Move_STARIGHT_COST);       // 6
+        TryAddPathNode(centerX, bottomY, Move_STARIGHT_COST);      // 2
+        TryAddPathNode(centerX, topY, Move_STARIGHT_COST);         // 8
 
-        GetStraightNode(leftX, centerY);     // 4
-        GetStraightNode(rightX, centerY);     // 6
-        GetStraightNode(centerX, bottomY);     // 2
-        GetStraightNode(centerX, topY);     // 8
+        TryAddPathNode(leftX, bottomY, Move_DIAGONAL_COST);        // 1
+        TryAddPathNode(leftX, topY, Move_DIAGONAL_COST);           // 7 
+        TryAddPathNode(rightX, bottomY, Move_DIAGONAL_COST);       // 3
+        TryAddPathNode(rightX, topY, Move_DIAGONAL_COST);          // 9
     }
 
-    private void GetStraightNode(int x, int y)
+    private void TryAddPathNode(int x, int y, int moveCost)
     {
-        if (x < 0 || y < 0 || x >= 200 || y >= 200) return;
+        if ((x < 0 || y < 0 || x >= _nodeMapSize.x || y >= _nodeMapSize.y)) return;
 
         var node = _nodes[x, y];
-        if (_closeList.Contains(node)) return;
-        if (node.IsBlocked) return;
-        if (node.IsClosed) return;
+        if (_closeList.Contains(node) || node.IsBlocked) return;
+        if (moveCost == Move_DIAGONAL_COST)
+        {
+            var currentPos = _current.Pos;
+            var conorPos = node.Pos - currentPos;
+
+            if (_nodes[currentPos.x + conorPos.x, currentPos.y].IsBlocked
+             || _nodes[currentPos.x, currentPos.y + conorPos.y].IsBlocked)
+            {
+                return;
+            }
+        }
 
         if (_openList.Contains(node) == false)
         {
-            node.BeforeNode = _current;
-            node.G = Move_STARIGHT_COST + _current.G;
-            node.H = GetHeuristic(node.Pos, Goal);
-            node.F = node.G + node.H;
-            _openList.Enqueue(node);
+            AddNewNodeToOpenList(node, moveCost);
         }
-        else if (Move_STARIGHT_COST + _current.G + node.H < node.F)
+        else if (moveCost + _current.G + node.H < node.F)
         {
-            node.BeforeNode = _current;
-            node.G = Move_STARIGHT_COST + _current.G;
-            node.H = GetHeuristic(node.Pos, Goal);
-            node.F = node.G + node.H;
+            RefreshNode(node, moveCost);
         }
     }
 
-    private void GetDiagonalNode(int x, int y)
+    private void AddNewNodeToOpenList(PathNode node, int moveCost)
     {
-        if (x < 0 || y < 0 || x >= 200 || y >= 200) return;
-
-        var node = _nodes[x, y];
-        if (_closeList.Contains(node)) return;
-        if (node.IsBlocked) return;
-        if (node.IsClosed) return;
-
-        if (_openList.Contains(node) == false)
-        {
-            node.BeforeNode = _current;
-            node.G = Move_DIAGONAL_COST + _current.G;
-            node.H = GetHeuristic(node.Pos, Goal);
-            node.F = node.G + node.H;
-            _openList.Enqueue(node);
-        }
-        else if (Move_DIAGONAL_COST + _current.G + node.H <= node.F)
-        {
-            node.BeforeNode = _current;
-            node.G = Move_DIAGONAL_COST + _current.G;
-            node.H = GetHeuristic(node.Pos, Goal);
-            node.F = node.G + node.H;
-        }
+        node.H = CalculateHeuristic(node.Pos, _goal);
+        RefreshNode(node, moveCost);
+        _openList.Enqueue(node);
     }
 
-    private int GetHeuristic(Vector2Int start, Vector2Int end)
+    private void RefreshNode(PathNode node, int moveCost)
+    {
+        node.BeforeNode = _current;
+        node.G = moveCost + _current.G;
+        node.F = node.G + node.H;
+    }
+
+    private int CalculateHeuristic(Vector2Int start, Vector2Int end)
     {
         int disX = Mathf.Abs(start.x - end.x);
         int disY = Mathf.Abs(start.y - end.y);
-        int remain = Mathf.Abs(disX - disY);
-
-        return Move_DIAGONAL_COST * Mathf.Min(disX, disY) + Move_STARIGHT_COST * remain;
+        return (disX + disY) << _weight;
     }
 
-    private void LinkPath(PathNode node)
+    private void ConnectPathNode(PathNode node)
     {
-        _stack = new();
+        _stack.Push(node);
         while (node.BeforeNode != null)
         {
             node = node.BeforeNode;
@@ -177,8 +166,8 @@ public class Pathfinding : MonoBehaviour
         }
         for (int i = 0; i < DebugList.Count - 1; i++)
         {
-            Vector3 from = new Vector3(DebugList[i].x, DebugList[i].y);
-            Vector3 to = new Vector3(DebugList[i + 1].x, DebugList[i + 1].y);
+            Vector3 from = new Vector3(DebugList[i].x + 0.5f, DebugList[i].y + 0.5f);
+            Vector3 to = new Vector3(DebugList[i + 1].x + 0.5f, DebugList[i + 1].y + 0.5f);
             Gizmos.DrawLine(from, to);
         }
     }
